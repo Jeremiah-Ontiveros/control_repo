99f4231ab43af7f545e3a99cf767b29f3c9c524e
Jeremiah-Ontiveros <92655110+Jeremiah-Ontiveros@users.noreply.github.com>
2021-10-17 15:32:06 -0500

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,23 @@
+class minecraft {
+  file {'/opt/minecraft':
+    ensure => directory
+  }
+
+file {'/opt/minecraft/server.jar'}
+  ensure => file,
+  source => 'https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar'
+  
+  }
+
+packages {'java':
+  ensure => present,
+  }
+file {''/opt/minecraft/eula.txt':
+  ensure => file,
+  content => 'eula=true',
+  }
+  file {'/etc/systemd/system/minecraft.service'}
+    ensure => file,
+    source => 'puppet:///modules/mincraft/minecraft.service'
+    }
+ }
