e12815da60b0f40410e9759f8795bc7fcd2afd99
Jeremiah-Ontiveros <92655110+Jeremiah-Ontiveros@users.noreply.github.com>
2021-10-17 13:17:37 -0500

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -4,12 +4,7 @@ class profile::agent_nodes {
   dockeragent::node {'db.puppet.vm':}
   dockeragent::node {'minetest.puppet.vm':}
   host {'web.puppet.vm'
-    ensure => present,
-    ip     => 172.18.0.2
-    }
-  host {'db.puppet.vm
-    ensure => present
-    ip     => 172.18.0.3'}
+ 
     
     
 }
