5b0dd683972d7decdcb9ae503377dc8c929cd73e
Jeremiah-Ontiveros <92655110+Jeremiah-Ontiveros@users.noreply.github.com>
2021-10-17 01:04:34 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADABAAABAQCz1QCAR3Jy9tmtjmcG901a6iFwGbPVb993735Z7xvRYoDc3Qj1PWzcFiXeBalt+kQtggin+Vg9Uw0+93itlJNfTnHm1sflr7CJYhBcJ15XZVhXX3gAz4K4HFdWBXQFTu1E3Qz9turclwRXC1wcbFayGwgRFSZqPYVq0WwYTvFsLgFeZo4ZpKvXDprbtQ2k9qEJcLdTa6SyLGo/uBoDlp8dAAtmsugin7y8nyCsseObj1N1HsssVz7oaqObMLs5LTFS5AjekMy0rllhyEYRn2txIuUcYA+CbxgxLF9ivO1HuhgbEb81nRbUEqJGVki0W5BDuxZfWc9oeVAAv1Jxd',
 	}  
 }
