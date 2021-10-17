d0985b93b26a721a375a54098016ccdfa97dd6ec
Jeremiah-Ontiveros <92655110+Jeremiah-Ontiveros@users.noreply.github.com>
2021-10-17 15:59:07 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -3,7 +3,7 @@ class minecraft {
     ensure => directory
   }
 
-file {'/opt/minecraft/server.jar'}
+file {'/opt/minecraft/server.jar':
   ensure => file,
   source => 'https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar'
   
@@ -16,7 +16,7 @@ file {''/opt/minecraft/eula.txt':
   ensure => file,
   content => 'eula=true',
   }
-  file {'/etc/systemd/system/minecraft.service'}
+  file {'/etc/systemd/system/minecraft.service':
     ensure => file,
     source => 'puppet:///modules/mincraft/minecraft.service'
     }
